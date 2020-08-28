b717f20c964abe1a1d8339c4ea3e6dc1df8d0f2d
sjsdfg <736777445@qq.com>
2020-08-28 23:03:18 +0800

    feature:add compare_int.go

---
+++ com.github.sjsjdfg/CompareUtils/compare_int.go
@@ -0,0 +1,27 @@
+package CompareUtils
+
+func MaxInt(list ...int) int {
+	if len(list) <= 0 {
+		return 0
+	}
+	max := list[0]
+	for _, val := range list {
+		if val > max {
+			max = val
+		}
+	}
+	return max
+}
+
+func MinInt(list ...int) int {
+	if len(list) <= 0 {
+		return 0
+	}
+	min := list[0]
+	for _, val := range list {
+		if val < min {
+			min = val
+		}
+	}
+	return min
+}
