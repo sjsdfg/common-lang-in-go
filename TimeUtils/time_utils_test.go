58c18d6bfcb21ed89b9039a5a1b47d58978cf48c
sjsdfg <736777445@qq.com>
2020-09-02 14:19:45 +0800

    feature:time_utils_test.go

---
+++ TimeUtils/time_utils_test.go
@@ -0,0 +1,19 @@
+package TimeUtils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestMax(t *testing.T) {
+	now := time.Now()
+	after100 := now.AddDate(100, 0, 0)
+	assert.Equal(t, after100, Max(Zero, now, after100))
+}
+
+func TestMin(t *testing.T) {
+	now := time.Now()
+	after100 := now.AddDate(100, 0, 0)
+	assert.Equal(t, Zero, Min(Zero, now, after100))
+}
