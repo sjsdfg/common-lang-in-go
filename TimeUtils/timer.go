4402200325db9bf18a4c13160a644b04a906fd2e
sjsdfg <736777445@qq.com>
2020-09-09 10:41:20 +0800

    bugfix:fix timer

--- TimeUtils/timer.go
+++ TimeUtils/timer.go
@@ -13,18 +13,18 @@ func NewTimer() Timer {
 	return Timer{startTime: time.Now()}
 }
 
-func (t Timer) GetDurationInSeconds() int64 {
+func (t *Timer) GetDurationInSeconds() int64 {
 	return time.Now().Unix() - t.startTime.Unix()
 }
 
-func (t Timer) GetDurationInMills() int64 {
+func (t *Timer) GetDurationInMills() int64 {
 	return t.GetDurationInNanos() / DateUtils.NsPerMill
 }
 
-func (t Timer) GetDurationInNanos() int64 {
+func (t *Timer) GetDurationInNanos() int64 {
 	return time.Now().UnixNano() - t.startTime.UnixNano()
 }
 
-func (t Timer) Reset() {
+func (t *Timer) Reset() {
 	t.startTime = time.Now()
 }
