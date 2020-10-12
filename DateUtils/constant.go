02230f91f60325981697e3dbc7b398ab8b24527b
sjsdfg <736777445@qq.com>
2020-10-12 11:13:00 +0800

    feature:add time constant

---
+++ DateUtils/constant.go
@@ -0,0 +1,13 @@
+package DateUtils
+
+//time
+const (
+	YYYYMMDD = "20060102"
+	YYMMDDHH = "06010215"
+	YYMMDD   = "060102"
+	YYMM     = "0601"
+
+	YYYY_MM_DD = "2006-01-02"
+	YY_MM_DD   = "06-01-02"
+	YYMM_DD_HH = "06-01-02-15"
+)
