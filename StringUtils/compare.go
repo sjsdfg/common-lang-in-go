1b629eb8d7a4934277e34357e60a9f7375faef03
sjsdfg <736777445@qq.com>
2021-03-16 13:53:47 +0800

    feature:add CompareDigital

---
+++ StringUtils/compare.go
@@ -0,0 +1,14 @@
+package StringUtils
+
+// 假设：入参 a 和 b 都是合法的整数类型
+// 并且没有多余的前导 0
+func CompareDigital(a, b string) bool {
+	lenA := len(a)
+	lenB := len(b)
+	if lenA == lenB {
+		// 长度相等根据字符串判断
+		return a > b
+	}
+	// 不等根据长度判断
+	return lenA > lenB
+}
