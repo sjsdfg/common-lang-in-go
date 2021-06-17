4daec4c4cfb0a2e9a6112c22a539df44c3e7025f
sjsdfg <736777445@qq.com>
2021-06-17 16:50:50 +0800

    bugfix:fix assert

--- TimeUtils/time_apart_test.go
+++ TimeUtils/time_apart_test.go
@@ -1,9 +1,10 @@
 package TimeUtils
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDaysApart(t *testing.T) {
@@ -14,4 +15,12 @@ func TestDaysApart(t *testing.T) {
 
 	assert.Equal(t, int64(-7), DaysApart(testCase1[0], testCase1[1]))
 	assert.Equal(t, int64(7), DaysApart(testCase1[1], testCase1[0]))
+}
+
+func TestHoursApart(t *testing.T) {
+	utcTime := time.Date(2020, time.December, 8, 20, 35, 0, 0, time.UTC)
+	localTime := time.Date(2020, time.December, 8, 20, 35, 0, 0, time.Local)
+
+	assert.EqualValues(t, 8, HoursApart(utcTime, localTime))
+	assert.EqualValues(t, -8, HoursApart(localTime, utcTime))
 }
