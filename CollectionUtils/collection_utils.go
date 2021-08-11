e4e4d636192e0e5971a7f7c7b63daec24a7632f8
sjsdfg <736777445@qq.com>
2021-08-11 14:26:48 +0800

    feature:add indexOf

--- CollectionUtils/collection_utils.go
+++ CollectionUtils/collection_utils.go
@@ -37,3 +37,19 @@ func ForEach(list interface{}, action func(i int)) {
 func Sort(list interface{}, less func(i, j int) bool) {
 	sort.SliceStable(list, less)
 }
+
+func IndexOf(list interface{}, match func(i int) bool) int {
+	if list == nil || match == nil {
+		return -1
+	}
+	value := reflect.ValueOf(list)
+	if kind := value.Type().Kind(); kind != reflect.Array && kind != reflect.Slice {
+		return -1
+	}
+	for i := 0; i < value.Len(); i++ {
+		if find := match(i); find {
+			return i
+		}
+	}
+	return -1
+}
