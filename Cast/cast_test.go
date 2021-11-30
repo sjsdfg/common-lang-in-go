3878cdaf320977d94c4dfff17b1b4c2b1fbd10df
sjsdfg <736777445@qq.com>
2021-11-30 14:56:23 +0800

    feature:test

--- Cast/cast_test.go
+++ Cast/cast_test.go
@@ -3,7 +3,19 @@ package Cast
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestName(t *testing.T) {
+	num := 5
+	assert.Equal(t, ToString(1), fmt.Sprint(1))
+	t.Log(ToString(1), fmt.Sprint(1))
+	assert.Equal(t, ToString(20.5), fmt.Sprint(20.5))
+	t.Log(ToString(20.5), fmt.Sprint(20.5))
+	assert.Equal(t, ToString(&num), fmt.Sprint(num))
+	t.Log(ToString(&num), fmt.Sprint(num))
+}
 
 func BenchmarkName(b *testing.B) {
 	num := 5
